ec04f2446e37dd3ffcb560fbd7a6ffd64fc94051
nokx <72279573+nokx5@users.noreply.github.com>
2021-06-08 10:17:42 +0200

    add qn block

--- src/fhmdot/include/qnum.hpp
+++ src/fhmdot/include/qnum.hpp
@@ -1,82 +1,35 @@
 #pragma once
 
 #include <tuple>
+#include <cmath>
 
 #include "babel_type.hpp"
 
-template <class T> struct quantum_number_crtp {
+template <class T>
+struct quantum_number_crtp
+{
+  static constexpr index_t dim = T::qn_dim;
   inline static index_t sum(index_t q1, index_t q2) { return T::qsum(q1, q2); }
   inline static index_t sub(index_t q1, index_t q2) { return T::qsub(q1, q2); }
 };
 
-struct sh_none : quantum_number_crtp<sh_none> {
+struct sh_none : quantum_number_crtp<sh_none>
+{
+  static constexpr index_t qn_dim = 1;
   inline static index_t qsum(index_t q1, index_t q2) { return 0; }
-
   inline static index_t qsub(index_t q1, index_t q2) { return 0; }
 };
 
-struct sh_uone : quantum_number_crtp<sh_uone> {
+struct sh_u1 : quantum_number_crtp<sh_u1>
+{
+  static constexpr index_t qn_dim = 2;
   inline static index_t qsum(index_t q1, index_t q2) { return q1 + q2; }
-
   inline static index_t qsub(index_t q1, index_t q2) { return q1 - q2; }
-
-  // inline static index_t combine()
 };
 
-// struct sh_sutwo : quantum_number_crtp<sh_sutwo>
-// {
-//   static index_t sum(index_t q1, index_t q2) {
-//     return q1+q2;
-//   }
-//
-// };
-
-#if QNAME == QNUM_SPIN_HALF_NONE
-// local quantum number : 0
-constexpr index_t qn_total = 1;
-constexpr index_t qsum(index_t q1, index_t q2) { return 0; }
-constexpr index_t qsub(index_t q1, index_t q2) { return 0; }
-// qmap : qdeg, qn_left, qn_right, qn_combined, qn_combined_offset
-// constexpr std::array<std::tuple<index_t, index_t, index_t, index_t>,
-// qn_total> qmap[1] = { {2, 0, 0, 0, 0} };
-#elif QNAME == QNUM_SPIN_HALF_U1
-// local quantum number
-// q1 = 0 is sz=-1/2
-// q2 = 1 is sz=+1/2
-/* 'sh-U1'   : {'zero' : [(0,)], 'qn' : [(-1,),(1,)], 'deg' : [1,1], '2qn' :
-   [(-2,),(0,),(2,)], '2deg' : [1,2,1],
-   'map'     : [[(0,0),(0,1),(1,0),(1,1)] , [1,0,2,1],
-   [slice(0,1),slice(0,1),slice(0,1),slice(1,2)]] }, # S^z_tot*/
-
-constexpr index_t qn_total = 2;
-
-constexpr index_t static inline qsum(constexpr index_t q1,
-                                     constexpr index_t q2) {
-  return q1 + q2;
-}
-constexpr index_t static inline qsub(constexpr index_t q1,
-                                     constexpr index_t q2) {
-  return q1 - q2;
-}
-// qmap : qdeg, qn_left, qn_right, qn_combined, qn_combined_offset
-constexpr std::array<std::tuple<index_t, index_t, index_t, index_t>, qn_total>
-    qmap[4] = {{1, 0, 0, 1, 0}, // deg=1, down, down => 0
-               {1, 1, 1, 1, 1},
-               {1, 1, 1, 1, 1},
-               {1, 1, 1, 1, 1}};
-
-#elif QNAME == QNUM_SPIN_HALF_SU2
-
-#endif
-
-// constexpr index_t static inline
-// local_qnum(constexpr index_t left_index, constexpr index_t right_index) {
-// return
-//
-//     if (right_index == index_t::max_value) {
-//       return
-//     } else if (left_index == index_t::max_value) {
-//
-//     } else {
-//       return right_index - left_index;
-//     }
+struct sh_su2 : quantum_number_crtp<sh_su2>
+{
+  static constexpr index_t qn_dim = 1;
+  inline static index_t qsum(index_t q1, index_t q2) { return q1 + q2; }
+  inline static index_t qsub(index_t q1, index_t q2) { return q1 - q2; }
+};
--- tests/cpp/unit/test_qnum.cpp
+++ tests/cpp/unit/test_qnum.cpp
@@ -6,10 +6,17 @@
 // using namespace qnum;
 
 BOOST_AUTO_TEST_CASE(unittest_qnum_sh) {
+  BOOST_CHECK(quantum_number_crtp<sh_none>::dimension == 1);
   BOOST_CHECK(quantum_number_crtp<sh_none>::sum(2, 3) == 0);
   BOOST_CHECK(quantum_number_crtp<sh_none>::sum(7, 2) == 0);
 
-  BOOST_CHECK(quantum_number_crtp<sh_uone>::sum(0, 0) == 0);
-  BOOST_CHECK(quantum_number_crtp<sh_uone>::sum(1, 7) == 8);
-  BOOST_CHECK(quantum_number_crtp<sh_uone>::sum(4, 9) == 13);
+  BOOST_CHECK(quantum_number_crtp<sh_u1>::dimension == 2);
+  BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(0, 0) == 0);
+  BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(1, 7) == 8);
+  BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(4, 9) == 13);
+
+  BOOST_CHECK(quantum_number_crtp<sh_su2>::dimension == 1);
+  BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(0, 0) == 0);
+  BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(6, 7) == 13);
+  BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(4, 3) == 7);
 }
