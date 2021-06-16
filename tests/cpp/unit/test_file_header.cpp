ad9dbe6ec50a5884d4d2b25081198980624f2196
nokx <72279573+nokx5@users.noreply.github.com>
2021-06-16 23:03:00 +0200

    add file_header for files

---
+++ tests/cpp/unit/test_file_header.cpp
@@ -0,0 +1,9 @@
+#include <boost/test/unit_test.hpp>
+// #include "boost/list.hpp"
+
+//#include "fhmdot/include/file_header.hpp"
+
+BOOST_AUTO_TEST_CASE(unittest_file_header)
+{
+  //BOOST_CHECK(fhmdot::fheader::version() == "V:@PROJECT_VERSION@");
+}
