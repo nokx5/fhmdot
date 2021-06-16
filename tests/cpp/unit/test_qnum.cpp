077f6a45a3604780feae9a8c9ad90342c8891601
nokx <72279573+nokx5@users.noreply.github.com>
2021-06-16 23:01:45 +0200

    add qnum

--- tests/cpp/unit/test_qnum.cpp
+++ tests/cpp/unit/test_qnum.cpp
@@ -5,17 +5,18 @@
 
 // using namespace qnum;
 
-BOOST_AUTO_TEST_CASE(unittest_qnum_sh) {
-  BOOST_CHECK(quantum_number_crtp<sh_none>::dimension == 1);
+BOOST_AUTO_TEST_CASE(unittest_qnum_sh)
+{
+  BOOST_CHECK(quantum_number_crtp<sh_none>::dim == 1);
   BOOST_CHECK(quantum_number_crtp<sh_none>::sum(2, 3) == 0);
   BOOST_CHECK(quantum_number_crtp<sh_none>::sum(7, 2) == 0);
 
-  BOOST_CHECK(quantum_number_crtp<sh_u1>::dimension == 2);
+  BOOST_CHECK(quantum_number_crtp<sh_u1>::dim == 2);
   BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(0, 0) == 0);
   BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(1, 7) == 8);
   BOOST_CHECK(quantum_number_crtp<sh_u1>::sum(4, 9) == 13);
 
-  BOOST_CHECK(quantum_number_crtp<sh_su2>::dimension == 1);
+  BOOST_CHECK(quantum_number_crtp<sh_su2>::dim == 1);
   BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(0, 0) == 0);
   BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(6, 7) == 13);
   BOOST_CHECK(quantum_number_crtp<sh_su2>::sum(4, 3) == 7);
