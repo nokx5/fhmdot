bd9f264c6dfbe962c2e759934455257ba2ed4790
nokx <72279573+nokx5@users.noreply.github.com>
2021-06-25 17:13:51 +0200

    add generic mps to pybind11

--- src/pybind/hmatrix.hpp
+++ src/pybind/hmatrix.hpp
@@ -2,30 +2,39 @@
 
 #pragma once
 
-#include "fhmdot/include/yofi.hpp"
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+//#include <vector>
+
+// #include "fhmdot/include/yofi.hpp"
 
 namespace pybind {
+
+namespace hmatrix {
 template <class Q, typename T> class mps {
 public:
   typedef Q qnum_type;
   typedef T value_type;
+  typedef std::tuple<int, int, int> index_type;
+  // using numpy_array = py::array_t<T, py::array::c_style>;
 
-  // void from_python(std::list<std::tuple<>>){};
-  // void to_python(std::list<std::pair<>>){};
+  typedef pybind11::array_t<value_type, pybind11::array::c_style> numpy_array;
+  typedef typename std::list<std::pair<index_type, numpy_array>> hmat_type;
 
-  void load(std::string filename){
+  mps() {}
 
-  };
-  void save(std::string filename){
+  // void from_python(hmat_type mps_in){};
+  // hmat_type to_python(){};
 
-  };
+  // void load(std::string filename){};
+  // void save(std::string filename){};
 
 private:
-  // std::map<std::tuple<3>, std::vector<value_type>> arr_;
-}
+  hmat_type arr_;
+};
 
-template <class Q, typename T>
-class mpo {
+template <class Q, typename T> class mpo {
 
 public:
   typedef Q qnum_type;
@@ -34,15 +43,12 @@ public:
   // void from_python(std::list<std::tuple<>>)  {};
   // void to_python(std::list<std::pair<>>) {};
 
-  void load(std::string filename){
-
-  };
-  void save(std::string filename){
-
-  };
+  // void load(std::string filename){};
+  // void save(std::string filename){};
 
 private:
   // std::map<std::tuple<4>, std::vector<value_type>> arr_;
-}
+};
 
+} // namespace hmatrix
 } // namespace pybind
--- src/pybind/wrapper.cpp
+++ src/pybind/wrapper.cpp
@@ -2,12 +2,37 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "fhmdot/include/babel_type.hpp"
+#include "pybind/hmatrix.hpp"
 #include "pybind/utils.hpp"
+
+#include "fhmdot/include/babel_type.hpp"
+#include "fhmdot/include/qnum.hpp"
 
 namespace py = pybind11;
 
 template <typename T> using numpy_array = py::array_t<T, py::array::c_style>;
+
+template <typename T>
+void declare_generic_mps(py::module &m, std::string typestr) {
+  using tclass = T;
+  std::string pyclass_name = std::string("mps_") + typestr;
+  py::class_<tclass>(m, pyclass_name.c_str()).def(py::init<>())
+      // .def("set_defaults",
+      //      [](tclass &self, std::vector<std::string> &keys,
+      //      numpy_array<py_std::value_type> np_values) -> void {
+      //        py_std::size_type k =
+      //        py_std::size_type(np_values.request().shape[0]); if (k !=
+      //        keys.size())
+      //        {
+      //          throw
+      //          std::range_error(msg_pybind_dimension_mismatch_for({"keys",
+      //          "values.shape[0]"}));
+      //        }
+      //        auto *values = const_cast<py_std::value_type
+      //        *>(np_values.data()); self.py_set_defaults(k, values, keys);
+      //      })
+      ;
+}
 
 numpy_array<num_t> check(numpy_array<num_t> np_in) {
   py::buffer_info np_in_info = np_in.request();
@@ -19,6 +44,10 @@ numpy_array<num_t> check(numpy_array<num_t> np_in) {
   return np_out;
 }
 
+using mps_sh_none = pybind::hmatrix::mps<quantum_number_crtp<sh_none>, data_t>;
+using mps_sh_u1 = pybind::hmatrix::mps<quantum_number_crtp<sh_u1>, data_t>;
+using mps_sh_su2 = pybind::hmatrix::mps<quantum_number_crtp<sh_su2>, data_t>;
+
 PYBIND11_MODULE(fhm, m) {
   m.doc() = "Fast Hilbert Matrix Dot - a matrix representation (single matrix "
             "from MPS)";
@@ -27,4 +56,12 @@ PYBIND11_MODULE(fhm, m) {
   m.def("is_floating_precision", &is_floating_precision,
         "check if library precision is fixed to float 32 (if not, float64 is "
         "used)");
+  declare_generic_mps<mps_sh_none>(m, "sh_none");
+  declare_generic_mps<mps_sh_u1>(m, "sh_u1");
+  declare_generic_mps<mps_sh_su2>(m, "sh_su2");
+  m.def("mps",
+        [](std::string qname)
+            -> std::variant<mps_sh_none, mps_sh_u1, mps_sh_su2> {
+          return mps_sh_su2();
+        });
 }
