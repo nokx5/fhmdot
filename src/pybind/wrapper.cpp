469eb90ccc897ff129a7a1852661d7d36905ef5a
nokx <72279573+nokx5@users.noreply.github.com>
2021-06-26 08:44:05 +0200

    add variant choice

--- src/pybind/wrapper.cpp
+++ src/pybind/wrapper.cpp
@@ -10,12 +10,14 @@
 
 namespace py = pybind11;
 
-template <typename T> using numpy_array = py::array_t<T, py::array::c_style>;
+template <typename T>
+using numpy_array = py::array_t<T, py::array::c_style>;
 
 template <typename T>
-void declare_generic_mps(py::module &m, std::string typestr) {
+void declare_generic_mps(py::module &m, std::string typestr)
+{
   using tclass = T;
-  std::string pyclass_name = std::string("mps_") + typestr;
+  std::string pyclass_name = std::string("hmat_") + typestr;
   py::class_<tclass>(m, pyclass_name.c_str()).def(py::init<>())
       // .def("set_defaults",
       //      [](tclass &self, std::vector<std::string> &keys,
@@ -34,7 +36,8 @@ void declare_generic_mps(py::module &m, std::string typestr) {
       ;
 }
 
-numpy_array<num_t> check(numpy_array<num_t> np_in) {
+numpy_array<num_t> check(numpy_array<num_t> np_in)
+{
   py::buffer_info np_in_info = np_in.request();
   // np_in_info.view
   std::size_t n = np_in.size() - 2;
@@ -48,7 +51,8 @@ using mps_sh_none = pybind::hmatrix::mps<quantum_number_crtp<sh_none>, data_t>;
 using mps_sh_u1 = pybind::hmatrix::mps<quantum_number_crtp<sh_u1>, data_t>;
 using mps_sh_su2 = pybind::hmatrix::mps<quantum_number_crtp<sh_su2>, data_t>;
 
-PYBIND11_MODULE(fhm, m) {
+PYBIND11_MODULE(fhm, m)
+{
   m.doc() = "Fast Hilbert Matrix Dot - a matrix representation (single matrix "
             "from MPS)";
   m.def("check", &check,
@@ -59,9 +63,24 @@ PYBIND11_MODULE(fhm, m) {
   declare_generic_mps<mps_sh_none>(m, "sh_none");
   declare_generic_mps<mps_sh_u1>(m, "sh_u1");
   declare_generic_mps<mps_sh_su2>(m, "sh_su2");
-  m.def("mps",
-        [](std::string qname)
+  m.def("hmat",
+        [](std::string qname = "")
             -> std::variant<mps_sh_none, mps_sh_u1, mps_sh_su2> {
-          return mps_sh_su2();
-        });
+          if (qname == "sh_none")
+          {
+            return mps_sh_none();
+          }
+          else if (qname == "sh_u1")
+          {
+            return mps_sh_u1();
+          }
+          else if(qname == "sh_su2")
+          {
+            return mps_sh_su2();
+          }
+          else
+          {
+            throw std::range_error("Model Error.\n\t\t- all models are listed in the python module.");
+          }
+        },  py::arg("qname") = "sh_none");
 }
